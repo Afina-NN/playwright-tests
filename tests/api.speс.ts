cc3f3b0b957b39ce6c848628567c69569dccb6a3
Afina-NN <av09091981@gmail.com>
2025-10-27 18:50:16 +0300

    Api автотесты: create, update

--- tests/api.speс.ts
+++ tests/api.speс.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
 
-let userid;
+let userid = "string";
 
  test('Create_User' , async ({ request }) => {
  const response = await request.post('https://petstore.swagger.io/v2/users', 
@@ -8,7 +8,8 @@ let userid;
     data: {
              "name": "Nina",
              "job": "baker",
-             "city": "Moscow"
+             "city": "Moscow",
+             "id": "411"
     },
      headers: {
              "Accept": "application/json"
@@ -19,5 +20,28 @@ expect(response.status()).toBe(201)
 
 const res = await response.json();
 userid = res.id;
+  
+ });
+
+ test('Update_User' , async ({ request }) => {
+ const response = await request.put('https://petstore.swagger.io/v2/users' + userid, 
+    { 
+    data: {
+             "name": "Irina",
+             "job": "engineer",
+             "city": "Moscow",
+             "id": "411"
+    },
+     headers: {
+             "Accept": "application/json"
+            }
+ });
+console.log(await response.json());
+expect(response.status()).toBe(200);
+
+const res = await response.json();
+// Проверка, что имя обновилось
+expect(res.name).toBe("Irina");
+expect(res.job).toBe("engineer");
   
  });
