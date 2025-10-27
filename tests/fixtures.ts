19a7639fac08ae00cc9bd5305b485bcd690a7e37
Afina-NN <av09091981@gmail.com>
2025-10-27 10:04:33 +0300

    Добавила негативные тесты

--- tests/fixtures.ts
+++ tests/fixtures.ts
@@ -10,30 +10,56 @@ export class RegistrationPage {
   nextstepButtonDisabled: Locator;
   textField: Locator;
   dropdownField: Locator;
+  dropdownFieldClick: Locator;
+  genderDropdown: Locator;
+  dropdownChoose: Locator;
+  citizenshipFieldClick: Locator;
+  fieldNameWarning: Locator
+  
+
 
   constructor(private page: Page) {
    this.checkboxes = this.page.locator('.custom-checkbox__inner'); 
    this.nextstepButtonDisabled = this.page.locator(".btn-cross-over.btn-cross-over--step1").locator("//button[@disabled='disabled']"); 
    this.textField = this.page.locator(".v-text-field__slot"); 
    this.dropdownField = this.page.locator(".v-select__slot"); 
+   this.dropdownFieldClick = this.page.locator(".select-register.select-gender");
+   this.genderDropdown = this.page.locator(".v-menu__content");
+   this.dropdownChoose = this.page.locator(".v-list-item__content");
+   this.citizenshipFieldClick = this.page.locator(".select-register-citizenship")
+   this.fieldNameWarning = this.page.locator(".auth-form__input.text-field_required.error--text");
   }
 
-  async textFieldsValidation() {
+  async fieldsValidation() {
+  //поля с заполнением
   await expect(this.textField.getByLabel(email)).toBeVisible();
+  await expect(this.textField.getByLabel(name)).toBeVisible();
   await expect(this.textField.getByLabel(family)).toBeVisible();
   await expect(this.textField.getByLabel(surname)).toBeVisible();
   await expect(this.textField.getByLabel(birthDate)).toBeVisible();
-  }
-
-  async selectFieldsValidation() {
+  //поля с выпадашками
   await expect(this.dropdownField.getByLabel(gender)).toBeVisible();
   await expect(this.dropdownField.getByLabel(citizenship)).toBeVisible();
   }
 
-  async fullfillAllFields(name: string) {
+  async fillFields() {
+  await this.textField.getByLabel(email).fill("123asd4567$@gmail.com");
+  await this.textField.getByLabel(name).fill("Ирина");
+  await this.textField.getByLabel(family).fill("Бугрова");
+  await this.textField.getByLabel(surname).fill("Анатольевна");
+  await this.textField.getByLabel(birthDate).fill("09.09.1981");
 
+  await this.dropdownFieldClick.click();
+  await expect(this.genderDropdown).toBeVisible();
+  await this.dropdownChoose.filter({hasText: "Женский"}).click();
+
+  await this.citizenshipFieldClick.click();
+  await this.dropdownField.getByLabel(citizenship).fill("Россия");
+  await this.dropdownChoose.nth(2).click()
   }
   }
+  // прогнать тесты 10 раз подряд
+  // настроить параллельный прогон
 
 
 
--- tests/registrationForm.spec.ts
+++ tests/registrationForm.spec.ts
@@ -2,27 +2,22 @@ import { test, expect } from '@playwright/test';
 import { RegistrationPage } from './fixtures';
 import { email, family, name, surname, birthDate, gender, citizenship } from './fixtures';
 
- test.beforeEach("навигация на исходную страницу", async ({ page }) => {
+test.beforeEach("навигация на исходную страницу", async ({ page }) => {
     await page.goto('https://pk.hse.ru/auth/register');
   });
 
 test.describe('Позитивные тесты_форма регистрации', () => {
-test('закрыть форму', async ({ page }) => {
-await page.locator(".auth-navigation").getByRole( "link").locator(".auth-navigation__close").click();
-const registrationPage = new RegistrationPage(page);
+  test('закрыть форму', async ({ page }) => {
+    await page.locator(".auth-navigation").getByRole( "link").locator(".auth-navigation__close").click();
+    const registrationPage = new RegistrationPage(page);
 
  //валидация, что форма закрылась - кнопки "Следующий шаг" на странице больше нет
 await expect(registrationPage.nextstepButtonDisabled).not.toBeVisible();
   });
 
   test('проверка, что при открытии формы все поля пустые' , async ({ page }) => {
-const registrationPage = new RegistrationPage(page);
-
- //поля с заполнением пустые
- await registrationPage.textFieldsValidation();
-
- //поля с выпадашками не заполнены
- await registrationPage.selectFieldsValidation(); 
+    const registrationPage = new RegistrationPage(page);
+      await registrationPage.fieldsValidation();
 
  //чекбоксы неактивны
  await expect(registrationPage.checkboxes.first()).not.toBeChecked();
@@ -30,11 +25,68 @@ const registrationPage = new RegistrationPage(page);
 
  //кнопка "Следующий шаг" неактивна
  await expect(registrationPage.nextstepButtonDisabled).toBeVisible();
-  
 });
 
   test('заполнить все поля', async ({ page }) => {
-  const registrationPage = new RegistrationPage(page); 
+    const registrationPage = new RegistrationPage(page); 
+      await registrationPage.fillFields();
+
+  //нажать на чекбоксы
+  await registrationPage.checkboxes.first().check();
+  await registrationPage.checkboxes.nth(1).check();
+
+  //валидация, что кнопка Следующий шаг активна
+  await expect(registrationPage.nextstepButtonDisabled).not.toBeVisible();
+  //Нажать на кнопку Следующий шаг
+  await page.getByRole("button").filter({hasText: "Следующий шаг"}).click()
+  //валидация, что открылась 2 страница
+  await page.waitForSelector(".wrapper-confirmation-code");
   });
 });
+
+test.describe('Негативные тесты_форма регистрации', () => {
+  test('Проверка поля Имя', async ({ page }) => {
+    const registrationPage = new RegistrationPage(page); 
+    //ввести цифры
+    await registrationPage.textField.getByLabel(name).fill("23456777")
+    //валидация ворнинга
+    await expect(registrationPage.fieldNameWarning.filter({hasText:"Введите ФИО кириллицей"})).toBeVisible();   
+    await registrationPage.textField.getByLabel(name).fill("");
+
+    //ввести английские буквы
+    await registrationPage.textField.getByLabel(name).fill("asdsdsfdgfgff");
+    //валидация ворнинга
+    await expect(registrationPage.fieldNameWarning.filter({hasText:"Введите ФИО кириллицей"})).toBeVisible();   
+    
+    //нет ограничения на min и max количество символов 
+
+  }
+)
+ test('Заполнить все поля, кроме поля Имя', async ({ page }) => {
+    const registrationPage = new RegistrationPage(page); 
+   
+  await registrationPage.textField.getByLabel(email).fill("123@gmail.com");
+  await registrationPage.textField.getByLabel(family).fill("Бугрова");
+  await registrationPage.textField.getByLabel(surname).fill("Анатольевна");
+  await registrationPage.textField.getByLabel(birthDate).fill("09.09.1981");
+
+  await registrationPage.dropdownFieldClick.click();
+  await expect(registrationPage.genderDropdown).toBeVisible();
+  await registrationPage.dropdownChoose.filter({hasText: "Женский"}).click();
+
+  await registrationPage.citizenshipFieldClick.click();
+  await registrationPage.dropdownField.getByLabel(citizenship).fill("Россия");
+  await registrationPage.dropdownChoose.nth(2).click()
+
+  //нажать на чекбоксы
+  await registrationPage.checkboxes.first().check();
+  await registrationPage.checkboxes.nth(1).check();
+
+  //валидация, что кнопка Следующий шаг неактивна
+  await expect(registrationPage.nextstepButtonDisabled).toBeVisible(); 
+
+  }
+)
+}
+)
 
