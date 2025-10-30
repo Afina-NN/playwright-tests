81f584a63ce6844a6d23c03c556eb7d157b269aa
Afina-NN <av09091981@gmail.com>
2025-10-30 11:25:01 +0300

    registrationForm.spec.ts, Object + beforEach.spec.ts - оптимизация

--- tests/Object + beforeEach.spec.ts
+++ tests/Object + beforeEach.spec.ts
@@ -1,5 +1,4 @@
 import { test, expect, Page, Locator } from '@playwright/test';
-import { RegistrationPage } from './fixtures';
 
     const fields = [
       {
@@ -27,16 +26,6 @@ import { RegistrationPage } from './fixtures';
         name: "Дата рождения",
         text: "01.01.2000",
       },
-      {
-        locator: (page: Page): Locator => page.locator('.v-select__slot').getByLabel("Пол"),
-        name: "Пол",
-        text: "Женский",
-      },
-      {
-        locator: (page: Page): Locator => page.locator('.v-select__slot').getByLabel("Гражданство"),
-        name: "Гражданство",
-        text:"Российская федерация",
-      },
     ];
 
 test.beforeEach("навигация на исходную страницу", async ({ page }) => {
@@ -54,15 +43,11 @@ test.describe('Позитивные тесты_форма регистрации
 }); 
 
 test('заполнение полей' , async ({ page }) => {
-  const registrationPage = new RegistrationPage(page);
-
   fields.forEach(({locator, name, text})=>{
     test.step(`заполнение поля ${name}` , async () => {
       await locator(page).fill(text);
      });
     });
-    await registrationPage.dropdownField.getByLabel("Пол").click();
-    await page.locator(".v-list-item__content").filter({hasText: "Женский"}).click();
   });
 });
 
--- tests/registrationForm.spec.ts
+++ tests/registrationForm.spec.ts
@@ -3,22 +3,22 @@ import { RegistrationPage } from './fixtures';
 import { email, familyName, name, surname, birthDate, gender, citizenship, emailValue, nameValue, 
         surnameValue, birthDateValue, genderValue, citizenshipValue, warningFieldName } from './fixtures';
 
-test.beforeEach("навигация на исходную страницу", async ({ page }) => {
-    await page.goto('https://pk.hse.ru/auth/register');
+test.describe('Позитивные тесты_форма регистрации', () => {
+   let registrationPage: RegistrationPage;
+
+   test.beforeEach("навигация на исходную страницу", async ({ page }) => {
+     await page.goto('https://pk.hse.ru/auth/register');
+       registrationPage = new RegistrationPage(page);
   });
 
-test.describe('Позитивные тесты_форма регистрации', () => {
-  test('закрыть форму', async ({ page }) => {
-    const registrationPage = new RegistrationPage(page);
-      await registrationPage.navigationMenu.getByRole( "link").locator(".auth-navigation__close").click();
-    
+test('закрыть форму', async ({ page }) => {
+ await registrationPage.navigationMenu.getByRole( "link").locator(".auth-navigation__close").click();
  //валидация, что форма закрылась - кнопки "Следующий шаг" на странице больше нет
-await expect(registrationPage.nextstepButtonDisabled).not.toBeVisible();
+ await expect(registrationPage.nextstepButtonDisabled).not.toBeVisible();
   });
 
   test('проверка, что при открытии формы все поля пустые' , async ({ page }) => {
-    const registrationPage = new RegistrationPage(page);
-      await registrationPage.fieldsValidation();
+ await registrationPage.fieldsValidation();
 
  //чекбоксы неактивны
  await expect(registrationPage.checkboxes.first()).not.toBeChecked();
@@ -28,74 +28,77 @@ await expect(registrationPage.nextstepButtonDisabled).not.toBeVisible();
  await expect(registrationPage.nextstepButtonDisabled).toBeVisible();
 });
 
-  test('заполнить все поля', async ({ page }) => {
-    const registrationPage = new RegistrationPage(page); 
+test('заполнить все поля', async ({ page }) => {
 
-  //заполнить инпуты и дропдауны
-  await registrationPage.fillFields();
+ //заполнить инпуты и дропдауны
+ await registrationPage.fillFields();
 
-  //нажать на чекбоксы
-  await registrationPage.checkboxes.first().check();
-  await registrationPage.checkboxes.nth(1).check();
+ //нажать на чекбоксы
+ await registrationPage.checkboxes.first().check();
+ await registrationPage.checkboxes.nth(1).check();
 
-  //валидация, что кнопка Следующий шаг активна
-  await expect(registrationPage.nextstepButtonDisabled).not.toBeVisible();
+ //валидация, что кнопка Следующий шаг активна
+ await expect(registrationPage.nextstepButtonDisabled).not.toBeVisible();
 
-  //Нажать на кнопку Следующий шаг
-  await registrationPage.nextStepButton.click();
+ //Нажать на кнопку Следующий шаг
+ await registrationPage.nextStepButton.click();
   });
 });
 
 test.describe('Негативные тесты_форма регистрации', () => {
-  test('Проверка поля Имя', async ({ page }) => {
-    const registrationPage = new RegistrationPage(page); 
+   let registrationPage: RegistrationPage;
 
-    //ввести цифры
-    await registrationPage.textField.getByLabel(name).fill("23456777")
+   test.beforeEach("навигация на исходную страницу", async ({ page }) => {
+     await page.goto('https://pk.hse.ru/auth/register');
+       registrationPage = new RegistrationPage(page);
+  });
 
-    //валидация ворнинга
-    await expect(registrationPage.fieldNameWarning.filter({hasText: warningFieldName})).toBeVisible();  
+test('Проверка поля Имя', async ({ page }) => {
+ //ввести цифры
+ await registrationPage.textField.getByLabel(name).fill("23456777")
+
+ //валидация ворнинга
+ await expect(registrationPage.fieldNameWarning.filter({hasText: warningFieldName})).toBeVisible();  
     
-    await registrationPage.textField.getByLabel(name).fill("");
+ await registrationPage.textField.getByLabel(name).fill("");
 
-    //ввести английские буквы
-    await registrationPage.textField.getByLabel(name).fill("asdsdsfdgfgff");
+ //ввести английские буквы
+ await registrationPage.textField.getByLabel(name).fill("asdsdsfdgfgff");
 
-    //валидация ворнинга
-    await expect(registrationPage.fieldNameWarning.filter({hasText: warningFieldName})).toBeVisible(); 
+ //валидация ворнинга
+ await expect(registrationPage.fieldNameWarning.filter({hasText: warningFieldName})).toBeVisible(); 
     
-    //ввести символы
-    await registrationPage.textField.getByLabel(name).fill("№%?*");
+ //ввести символы
+ await registrationPage.textField.getByLabel(name).fill("№%?*");
 
-    //валидация ворнинга
-    await expect(registrationPage.fieldNameWarning.filter({hasText: warningFieldName})).toBeVisible(); 
+ //валидация ворнинга
+ await expect(registrationPage.fieldNameWarning.filter({hasText: warningFieldName})).toBeVisible(); 
     
-    //нет ограничения на min и max количество символов 
-
+ //нет ограничения на min и max количество символов 
   }
 )
- test('Заполнить все поля, кроме поля Имя', async ({ page }) => {
-    const registrationPage = new RegistrationPage(page); 
-   
-  await registrationPage.textField.getByLabel(email).fill(emailValue);
-  await registrationPage.textField.getByLabel(familyName).fill(nameValue);
-  await registrationPage.textField.getByLabel(surname).fill(surnameValue);
-  await registrationPage.textField.getByLabel(birthDate).fill(birthDateValue);
+test('Заполнить все поля, кроме поля Имя', async ({ page }) => {
 
-  await registrationPage.dropdownFieldClick.click();
-  await expect(registrationPage.genderDropdown).toBeVisible();
-  await registrationPage.dropdownChoose.filter({hasText: genderValue}).click();
+ //заполнить текстовые поля
+ await registrationPage.textField.getByLabel(email).fill(emailValue);
+ await registrationPage.textField.getByLabel(familyName).fill(nameValue);
+ await registrationPage.textField.getByLabel(surname).fill(surnameValue);
+ await registrationPage.textField.getByLabel(birthDate).fill(birthDateValue);
 
-  await registrationPage.citizenshipFieldClick.click();
-  await registrationPage.dropdownField.getByLabel(citizenship).fill(citizenshipValue);
-  await registrationPage.dropdownChoose.nth(2).click()
+ //заполнить дропдауны
+ await registrationPage.dropdownFieldClick.click();
+ await expect(registrationPage.genderDropdown).toBeVisible();
+ await registrationPage.dropdownChoose.filter({hasText: genderValue}).click();
+ await registrationPage.citizenshipFieldClick.click();
+ await registrationPage.dropdownField.getByLabel(citizenship).fill(citizenshipValue);
+ await registrationPage.dropdownChoose.nth(2).click()
 
-  //нажать на чекбоксы
-  await registrationPage.checkboxes.first().check();
-  await registrationPage.checkboxes.nth(1).check();
+ //нажать на чекбоксы
+ await registrationPage.checkboxes.first().check();
+ await registrationPage.checkboxes.nth(1).check();
 
-  //валидация, что кнопка Следующий шаг неактивна
-  await expect(registrationPage.nextstepButtonDisabled).toBeVisible(); 
+ //валидация, что кнопка Следующий шаг неактивна
+ await expect(registrationPage.nextstepButtonDisabled).toBeVisible(); 
   }
 )
 }
