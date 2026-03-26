09ef88b1ae8d6a6b96c67ff4b73cbeddb0abdba1
Afina-NN <av09091981@gmail.com>
2026-03-26 17:50:51 +0300

    hse.spec.ts и mgu.spec.ts с фикстурами_применен prettier format

--- tests/hse.spec.ts
+++ tests/hse.spec.ts
@@ -1,72 +1,89 @@
 import { test, expect } from '@playwright/test';
-import {dropdownField, emptyFieldsValidation, fieldsName, fillBirthdateField, fillFields, locator, RegistrationPage, value, values } from './hse';
+import {
+  dropdownField,
+  emptyFieldsValidation,
+  fieldsName,
+  fillBirthdateField,
+  fillFields,
+  locator,
+  RegistrationPage,
+  value,
+  values,
+} from './hse';
 
 let registrationPage: RegistrationPage;
 
 test.beforeEach(async ({ page }) => {
-await page.goto('https://pk.hse.ru/auth/register');
-registrationPage = new RegistrationPage(page);
-  });
+  await page.goto('https://pk.hse.ru/auth/register');
 
-test("Валидация того, что все поля по дефолту пустые", async ({ page }) => {
-emptyFieldsValidation.forEach(element=>{
-test.step(`Пустое поле ${element.name}` , async () => {
-expect(page.getByLabel(element.name).locator(element.flag)).not.toBeVisible();
-  }
-)
-})
-// валидация чекбоксов
-test.step("Пустые чекбоксы" , async () => {
-await expect(registrationPage.checkBoxcheck.first()).not.toBeChecked();
-await expect(registrationPage.checkBoxcheck.nth(1)).not.toBeChecked();
-}
-)
-// валидация кнопки Следующий шаг
-test.step("Кнопка Следующий шаг неактивна" , async () => {
-await expect(page.getByRole("button", {name: value.nextStepButton})).toBeDisabled();
-}
-)
-}
-)
+  registrationPage = new RegistrationPage(page);
+});
+
+test('Валидация того, что все поля по дефолту пустые', async ({ page }) => {
+  emptyFieldsValidation.forEach((element) => {
+    test.step(`Пустое поле ${element.name}`, async () => {
+      expect(page.getByLabel(element.name).locator(element.flag)).not.toBeVisible();
+    });
+  });
+  // валидация чекбоксов
+  test.step('Пустые чекбоксы', async () => {
+    await expect(registrationPage.checkBoxcheck.first()).not.toBeChecked();
+    await expect(registrationPage.checkBoxcheck.nth(1)).not.toBeChecked();
+  });
+  // валидация кнопки Следующий шаг
+  test.step('Кнопка Следующий шаг неактивна', async () => {
+    await expect(page.getByRole('button', { name: value.nextStepButton })).toBeDisabled();
+  });
+});
 
 test.describe('Заполнение полей', () => {
-fillFields.forEach(element=>{
-test(`Поля заполнены ${element.testName}`, async ({ page }) => {
- await page.getByLabel(fieldsName.email).fill(element.email);
- await page.getByLabel(fieldsName.secondname).fill(element.secondname);
- await page.getByLabel(fieldsName.name).fill(element.name);
- await page.getByLabel(fieldsName.surname).fill(element.surname);
- await page.getByLabel(fieldsName.birthdate).fill(element.birthdate);
+  fillFields.forEach((element) => {
+    test(`Поля заполнены ${element.testName}`, async ({ page }) => {
+      await page.getByLabel(fieldsName.email).fill(element.email);
+      await page.getByLabel(fieldsName.secondname).fill(element.secondname);
+      await page.getByLabel(fieldsName.name).fill(element.name);
+      await page.getByLabel(fieldsName.surname).fill(element.surname);
+      await page.getByLabel(fieldsName.birthdate).fill(element.birthdate);
 
- // заполнение выпадашек
- await dropdownField(page, locator.dropdown.select.gender, fieldsName.gender, locator.dropdown.item.gender, values.fields.gender);
- await dropdownField(page, locator.dropdown.select.citizenship, fieldsName.citizenship, locator.dropdown.item.citizenship, values.fields.citizenship);
+      // заполнение выпадашек
+      await dropdownField(
+        page,
+        locator.dropdown.select.gender,
+        fieldsName.gender,
+        locator.dropdown.item.gender,
+        values.fields.gender,
+      );
+      await dropdownField(
+        page,
+        locator.dropdown.select.citizenship,
+        fieldsName.citizenship,
+        locator.dropdown.item.citizenship,
+        values.fields.citizenship,
+      );
 
- // заполнение чекбоксов
- await registrationPage.checkBoxcheck.first().click();
- await registrationPage.checkBoxcheck.nth(1).click();
- 
- // валидация кнопки Следуюший шаг
- if(element.expectedResult){
-   await expect(page.getByRole("button", {name: value.nextStepButton}).locator(locator.button.disabled)).not.toBeVisible();}
-   else{
-   await expect(page.getByRole("button", {name: value.nextStepButton}).locator(locator.button.disabled)).toBeVisible(); 
-   }
+      // заполнение чекбоксов
+      await registrationPage.checkBoxcheck.first().click();
+      await registrationPage.checkBoxcheck.nth(1).click();
 
-  })
-})
-})
+      // валидация кнопки Следуюший шаг
+      if (element.expectedResult) {
+        await expect(
+          page.getByRole('button', { name: value.nextStepButton }).locator(locator.button.disabled),
+        ).not.toBeVisible();
+      } else {
+        await expect(
+          page.getByRole('button', { name: value.nextStepButton }).locator(locator.button.disabled),
+        ).toBeVisible();
+      }
+    });
+  });
+});
 
 test.describe('Негативные кейсы на поле Дата рождения', () => {
-fillBirthdateField.forEach(element=>{
-test(`Негативный кейс поля дата рождения: ${element.testName}`, async ({ page }) => {
- await page.getByLabel(fieldsName.birthdate).fill(element.value);
- await expect(page.getByRole("alert").filter({hasText: element.warning})).toBeVisible();
-})
-})
-})
-
-
-
-
-
+  fillBirthdateField.forEach((element) => {
+    test(`Негативный кейс поля дата рождения: ${element.testName}`, async ({ page }) => {
+      await page.getByLabel(fieldsName.birthdate).fill(element.value);
+      await expect(page.getByRole('alert').filter({ hasText: element.warning })).toBeVisible();
+    });
+  });
+});
--- tests/hse.ts
+++ tests/hse.ts
@@ -1,110 +1,159 @@
 import { Locator, Page } from '@playwright/test';
 
-const alphabet = "абвгдежзклмнопрстуфхцчэюя"
-const ind = Math.ceil((Math.random()*10));
+export class RegistrationPage {
+  checkBoxcheck: Locator;
+
+  constructor(private page: Page) {
+    this.checkBoxcheck = this.page.locator(locator.checkbox.check);
+  }
+}
+
+const alphabet: string = 'абвгдежзклмнопрстуфхцчэюя';
+const ind: number = Math.ceil(Math.random() * 10);
 
 // название поля
 export const fieldsName = {
-    email: "E-mail",
-    secondname: "Фамилия",
-    name: "Имя",
-    surname: "Отчество",
-    birthdate: "Дата рождения",
-    gender: "Пол",
-    citizenship: "Гражданство",
-}
+  email: 'E-mail',
+  secondname: 'Фамилия',
+  name: 'Имя',
+  surname: 'Отчество',
+  birthdate: 'Дата рождения',
+  gender: 'Пол',
+  citizenship: 'Гражданство',
+} as const;
 
 // значения поля
 export const values = {
-    fields: {
-        email: `${Math.floor(Math.random() * 1000)}@gmail.com`,
-        secondname: "Иванов".concat(alphabet[ind]),
-        name: "Нин".concat(alphabet[ind]),
-        surname: "Ивановна",
-        birthdate: "09.09.1981",
-        gender: "Женский",
-        citizenship: "Россия",
-},
-    warning: {
-        date: "Неверный формат даты",
-    }
-
-}
+  fields: {
+    email: `${Math.floor(Math.random() * 1000)}@gmail.com`,
+    secondname: 'Иванов'.concat(alphabet[ind]),
+    name: 'Нин'.concat(alphabet[ind]),
+    surname: 'Ивановна',
+    birthdate: '09.09.1981',
+    gender: 'Женский',
+    citizenship: 'Россия',
+  },
+  warning: {
+    date: 'Неверный формат даты',
+  },
+} as const;
 
 // локаторы
 export const locator = {
-    checkbox:{
-      check: ".custom-checkbox__inner",
+  checkbox: {
+    check: '.custom-checkbox__inner',
+  },
+  button: {
+    disabled: "//button[@disabled='disabled']",
+  },
+  dropdown: {
+    select: {
+      gender: '.select-register.select-gender',
+      citizenship: '.select-register-citizenship',
     },
-    button: {
-      disabled: "//button[@disabled='disabled']",
+    item: {
+      gender: '.v-list-item--link',
+      citizenship: '.v-list-item__content',
     },
-    dropdown: {
-        select: {
-            gender: ".select-register.select-gender",
-            citizenship: ".select-register-citizenship",
-        },
-        item: {
-            gender: ".v-list-item--link",
-            citizenship: ".v-list-item__content",
-        }
-    },
-    flag: {
-            active: ".v-label--active",
-    }
-}
+  },
+  flag: {
+    active: '.v-label--active',
+  },
+} as const;
 
 export const value = {
-    nextStepButton: "Следующий шаг",
-}
+  nextStepButton: 'Следующий шаг',
+} as const;
 
 //валидация того, что поля не заполнены
-export const emptyFieldsValidation=[
-    {name: fieldsName.email, flag: locator.flag.active},
-    {name: fieldsName.secondname, flag: locator.flag.active},
-    {name: fieldsName.name, flag: locator.flag.active},
-    {name: fieldsName.surname, flag: locator.flag.active},
-    {name: fieldsName.birthdate, flag: locator.flag.active},
-    {name: fieldsName.gender, flag: locator.flag.active},
-    {name: fieldsName.citizenship, flag: locator.flag.active},
-]
+export const emptyFieldsValidation = [
+  { name: fieldsName.email, flag: locator.flag.active },
+  { name: fieldsName.secondname, flag: locator.flag.active },
+  { name: fieldsName.name, flag: locator.flag.active },
+  { name: fieldsName.surname, flag: locator.flag.active },
+  { name: fieldsName.birthdate, flag: locator.flag.active },
+  { name: fieldsName.gender, flag: locator.flag.active },
+  { name: fieldsName.citizenship, flag: locator.flag.active },
+];
 
 // заполнить поля input
 export const fillFields = [
-    {testName: "все ", email: values.fields.email, secondname: values.fields.secondname, name: values.fields.name, surname: values.fields.surname, birthdate: values.fields.birthdate, expectedResult: true},
-    {testName: ", кроме поля почта", email: "", secondname: values.fields.secondname, name: values.fields.name, surname: values.fields.surname, birthdate:values.fields.birthdate, warning: "Заполните E-mail", expectedResult: true},
-    {testName: ", кроме поля Фамилия", email: values.fields.email, secondname: "", name: values.fields.name, surname: values.fields.surname, birthdate: values.fields.birthdate, warning: "Заполните свои ФИО", expectedResult: true},
-    {testName: ", кроме поля Имя", email: values.fields.email, secondname: values.fields.secondname, name: "", surname: values.fields.surname, birthdate: values.fields.birthdate, warning: "Заполните свои ФИО", expectedResult: true},
-    {testName: ", кроме поля Дата Рождения", email: values.fields.email, secondname: values.fields.secondname, name: values.fields.name, surname: values.fields.surname, birthdate: "", warning: "Заполните свои ФИО", expectedResult: true},
-]
+  {
+    testName: 'все ',
+    email: values.fields.email,
+    secondname: values.fields.secondname,
+    name: values.fields.name,
+    surname: values.fields.surname,
+    birthdate: values.fields.birthdate,
+    expectedResult: true,
+  },
+  {
+    testName: ', кроме поля почта',
+    email: '',
+    secondname: values.fields.secondname,
+    name: values.fields.name,
+    surname: values.fields.surname,
+    birthdate: values.fields.birthdate,
+    warning: 'Заполните E-mail',
+    expectedResult: true,
+  },
+  {
+    testName: ', кроме поля Фамилия',
+    email: values.fields.email,
+    secondname: '',
+    name: values.fields.name,
+    surname: values.fields.surname,
+    birthdate: values.fields.birthdate,
+    warning: 'Заполните свои ФИО',
+    expectedResult: true,
+  },
+  {
+    testName: ', кроме поля Имя',
+    email: values.fields.email,
+    secondname: values.fields.secondname,
+    name: '',
+    surname: values.fields.surname,
+    birthdate: values.fields.birthdate,
+    warning: 'Заполните свои ФИО',
+    expectedResult: true,
+  },
+  {
+    testName: ', кроме поля Дата Рождения',
+    email: values.fields.email,
+    secondname: values.fields.secondname,
+    name: values.fields.name,
+    surname: values.fields.surname,
+    birthdate: '',
+    warning: 'Заполните свои ФИО',
+    expectedResult: true,
+  },
+];
 
 // негативные кейсы поля Дата рождения
 export const fillBirthdateField = [
-{testName: "неверное число ", value: "32.10.2000", warning: values.warning.date},
-{testName: "неверный месяц ", value: "31.13.2000", warning: values.warning.date},
-{testName: "заполнено только число ", value: "31", warning: values.warning.date},
-{testName: "год выше допустимого", value: "31.10.2012", warning: values.warning.date},
-{testName: "год ниже допустимого", value: "24.01.1909", warning: values.warning.date},
-{testName: "дата начинается с года", value: "1981.09.09", warning: values.warning.date},
-]
+  { testName: 'неверное число ', value: '32.10.2000', warning: values.warning.date },
+  { testName: 'неверный месяц ', value: '31.13.2000', warning: values.warning.date },
+  { testName: 'заполнено только число ', value: '31', warning: values.warning.date },
+  { testName: 'год выше допустимого', value: '31.10.2012', warning: values.warning.date },
+  { testName: 'год ниже допустимого', value: '24.01.1909', warning: values.warning.date },
+  { testName: 'дата начинается с года', value: '1981.09.09', warning: values.warning.date },
+];
 
 // заполнить все поля, кроме поля Пол
 export const fillWithoutGender = [
-{label: fieldsName.email, value: values.fields.email},
-{label: fieldsName.secondname, value: values.fields.secondname},
-{label: fieldsName.name, value: values.fields.name},
-]
-export class RegistrationPage {
-checkBoxcheck: Locator;
-
-constructor(private page: Page){
-this.checkBoxcheck = this.page.locator(locator.checkbox.check)
-}
-
-}
+  { label: fieldsName.email, value: values.fields.email },
+  { label: fieldsName.secondname, value: values.fields.secondname },
+  { label: fieldsName.name, value: values.fields.name },
+];
 
 // заполнить поля dropdown
-export async function dropdownField(page:Page, selectLocator: string,  selectText: string, itemLocator: string, itemText: string){
- await page.locator(selectLocator).filter({hasText: selectText}).click();
- await page.locator(itemLocator).filter({hasText: itemText}).click();
+export async function dropdownField(
+  page: Page,
+  selectLocator: string,
+  selectText: string,
+  itemLocator: string,
+  itemText: string,
+) {
+  await page.locator(selectLocator).filter({ hasText: selectText }).click();
+  await page.locator(itemLocator).filter({ hasText: itemText }).click();
 }
